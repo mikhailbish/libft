0fd60b3685963ac6e8fdc162c927507cc62b6a3c
Mikhail Butuzov <mbutuzov@c1r5p9.hive.fi>
2024-07-02 19:54:41 +0300

    include fix

--- ft_memset.c
+++ ft_memset.c
@@ -1,11 +1,23 @@
-#include <libft.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbutuzov <mbutuzov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/02 17:17:57 by mbutuzov          #+#    #+#             */
+/*   Updated: 2024/07/02 19:48:53 by mbutuzov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void put_ul(size_t len, void *ptr, unsigned long long num)
+#include "libft.h"
+
+void	put_ul(size_t len, void *ptr, unsigned long long num)
 {
-	unsigned long long *chunk;
+	unsigned long long	*chunk;
 
 	chunk = (unsigned long long *)ptr;
-	while(len > sizeof(unsigned long long))
+	while (len > sizeof(unsigned long long))
 	{
 		*chunk = num;
 		chunk++;
@@ -15,25 +27,25 @@ void put_ul(size_t len, void *ptr, unsigned long long num)
 
 void	*ft_memset(void *mem, int c, size_t len)
 {
-	char *str;
-	unsigned long long *chunk;
-	unsigned long long tmp;
-	int chunk_size;
+	unsigned long long	*chunk;
+	unsigned long long	tmp;
+	char				*str;
+	int					chunk_size;
 
 	tmp = 0;
 	chunk = (unsigned long long *)mem;
-	chunk_size = (int)sizeof(unsigned long long);
+	chunk_size = (int) sizeof(unsigned long long);
 	while (chunk_size > 0)
 	{
 		tmp += (unsigned long long)(unsigned char)c;
 		if (chunk_size > 1)
 			tmp = tmp << 8;
-		chunk_size--; 
+		chunk_size--;
 	}
 	if (len >= sizeof(unsigned long long))
 		put_ul(len, mem, tmp);
 	str = (unsigned char *)chunk;
-	while(len--)
+	while (len--)
 	{
 		*str = (unsigned char)c;
 		str++;
