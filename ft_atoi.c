f99d5281be14e2661a5fd32196a2bcd8d2300a27
Mikhail Butuzov <mbutuzov@c2r6p9.hive.fi>
2024-08-10 19:58:54 +0300

    made atoi use strtol

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,49 +6,13 @@
 /*   By: mbutuzov <mbutuzov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 19:35:44 by mbutuzov          #+#    #+#             */
-/*   Updated: 2024/07/30 19:35:49 by mbutuzov         ###   ########.fr       */
+/*   Updated: 2024/08/10 18:45:05 by mbutuzov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_getnum(const char *str, long long sign)
-{
-	long long	num;
-	long long	tmp;
-
-	num = 0;
-	while (ft_isdigit(*str))
-	{
-		tmp = num;
-		num += (long long)(((*str) - '0') * sign);
-		if (ft_isdigit(*(str + 1)))
-			num *= 10;
-		if (sign > 0 && num < tmp)
-			return (-1);
-		if (sign < 0 && num > tmp)
-			return (0);
-		str++;
-	}
-	return ((int)num);
-}
-
 int	ft_atoi(const char *str)
 {
-	long long	sign;
-
-	sign = 1;
-	while (*str == '\f' || *str == '\n' || *str == '\r'
-		|| *str == '\t' || *str == '\v' || *str == ' ')
-	{
-		str++;
-	}
-	if (*str == '-')
-	{
-		sign = -sign;
-		str++;
-	}
-	else if (*str == '+')
-		str++;
-	return (ft_getnum(str, sign));
+	return (ft_strtol(str, 0, 10));
 }
